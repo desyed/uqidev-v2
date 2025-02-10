8e161567029c1c6c46a519de626d89360867cebd
Syed Shihab <syedshihabdu@gmail.com>
2025-02-10 13:55:53 +0600

    update

--- src/app/homepage/WhyVanta.tsx
+++ src/app/homepage/WhyVanta.tsx
@@ -8,7 +8,7 @@ const WhyVanta = () => {
         <div>
             <div className='bg-gradient-to-b from-black to-neutral-800 rounded-3xl border border-neutral-800'>
                 <div className="bg-gradient-to-b from-neutral-950 to-neutral-800 rounded-2xl border border-neutral-800 shadow-md m-[6px] flex flex-col lg:flex-row space-y-4 p-3 md:px-8 py-16 ">
-                    <div className='lg:w-1/2 '>
+                    <div className=' '>
                         <GradientTitle text='Why Choose UQIDEV?' />
                         <p className="text-neutral-400">
                         🚀 Innovative Solutions – We craft cutting-edge software tailored to your business needs. <br />
@@ -25,10 +25,7 @@ const WhyVanta = () => {
 
                         </p>
                     </div>
-                    <div className='relative flex  gap-2 mt-12 justify-start md:justify-center items-center lg:w-1/2'>
-                        <Link href={'/about'}><Button className='bg-gradient-to-b from-neutral-400 via-neutral-500 to-neutral-600'>Learn More</Button></Link>
-                        <Link href={'/contact'}><Button className='border border-neutral-700 bg-gradient-to-b from-neutral-800 to-neutral-900'>Get in touch</Button></Link>
-                    </div>
+                    
                 </div>
             </div>
         </div>
